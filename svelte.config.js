098f7cbd5b4a723bb30c7b54affdea29ca6f4cef
RohanGudla <51538137+RohanGudla@users.noreply.github.com>
2024-07-12 15:26:05 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
     preprocess: preprocess(),
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        }),
+        paths: {
+            base: dev ? '' : '/rohangudla.github.io'
+        },
         inlineStyleThreshold: 10240,
         router: false,
         hydrate: false,
-
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
-
-        // Override http methods in the Todo forms
-        methodOverride: {
-            allowed: ['PATCH', 'DELETE']
-        }
+        target: '#svelte'
     }
 };
 
